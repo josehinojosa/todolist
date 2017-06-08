1a5b23a686aa99236bca9211290b36272b3db544
Joe <josehinojosa8@gmail.com>
2017-06-08 15:38:41 -0400

    Joes f'd up file

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// Add your JS here.
+
+    var addTaskButton = document.queryselector("#new-todo");
+    var newTaskInput = document.getElementById("#todoapp");
+    var todolistContainer = document.getElementById("#todo-list");
+
+    new-todo.addEventListener('keydown', function addListItem(event) {
+      var taskName = newTaskInput.value;
+      newTaskInput.value = "";
+      todolistContainer.insertAdjacentHTML('afterbegin', taskName);
+    });
+return event;
+}
